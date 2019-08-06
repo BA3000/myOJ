f5446c479273ddcf3c8c4e7f1161d688a514e8a4
BA3000 <2864281096@qq.com>
2019-08-06 23:06:13 +0800

    [BA] LeetCode 141
    判断链表是否有环

---
+++ leetcode/141_Linked_List_Cycle.cpp
@@ -0,0 +1,13 @@
+// 知识点：快慢指针、链表
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode *slow = head, *fast = head;
+        while(fast&&fast->next){
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow == fast) return true;
+        }
+        return false;
+    }
+};
